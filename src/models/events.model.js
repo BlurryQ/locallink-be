01bb7c91b30aa6eddad74acb5edd48da7b3b58f0
BlurryQ <jazz.barlow@outlook.com>
2025-02-26 17:00:02 +0000

    Adds and implements queries

--- src/models/events.model.js
+++ src/models/events.model.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
+const { Query } = require('node-appwrite');
 const { databases } = require('../config/appwrite');
 
-exports.selectAllEvents = async () => {
+exports.selectAllEvents = async (category, isFree = false) => {
+  // return queries in ascending order by default
+  const queries = [Query.orderAsc('start')];
+  if (category) queries.push(Query.equal('category', category));
+  if (isFree) queries.push(Query.equal('price', 0));
   return await databases.listDocuments(
     process.env.APPWRITE_DATABASE_ID,
-    process.env.APPWRITE_EVENTS_TABLE
+    process.env.APPWRITE_EVENTS_TABLE,
+    queries
   );
 };
 
