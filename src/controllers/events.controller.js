db95d6da5d55ee6c8db60a1fbea9ef6305ec2485
BlurryQ <jazz.barlow@outlook.com>
2025-02-27 15:18:46 +0000

    Implements location filtering

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -5,16 +5,30 @@ const {
   postEvent,
 } = require('../models/events.model');
 const { formatEventData } = require('../utils/formatEventData');
+const { haversineDistance } = require('../utils/haversineDistance');
 
 exports.getAllEvents = async (req, res) => {
-  const { category, location, isFree } = req.query;
+  const { category, lat, long, radius, isFree } = req.query;
   try {
     const data = await selectAllEvents(category, isFree);
-    const events = data.documents.map(formatEventData);
+    let events = data.documents.map(formatEventData);
+    // if location query and all data present
+    // TODO abstraction function
+    if (lat && long && radius) {
+      const currentCoords = { lat, long };
+      events = events.filter((event) => {
+        event.location = JSON.parse(event.location);
+        const distanceFromLocation = haversineDistance(
+          currentCoords,
+          event.location.coords
+        );
+        if (distanceFromLocation <= radius) return event;
+      });
+    }
     const allEvents = { events, total: data.total };
     res.status(200).send(allEvents);
   } catch (err) {
-    console.error('Error fetching events:', err);
+    // console.error('Error fetching events:', err);
     res.status(400).send({ error: err.message });
   }
 };
@@ -26,7 +40,7 @@ exports.getEventByID = async (req, res) => {
     const event = formatEventData(data);
     res.status(200).send(event);
   } catch (err) {
-    console.error('Error fetching event:', err);
+    // console.error('Error fetching event:', err);
     res.status(404).send({ error: err.message });
   }
 };
@@ -38,7 +52,7 @@ exports.createEvent = async (req, res) => {
     const newEvent = formatEventData(newEventData);
     res.status(201).send(newEvent);
   } catch (err) {
-    console.error('Error posting event:', err);
+    // console.error('Error posting event:', err);
     res.status(400).send({ error: err.message });
   }
 };
