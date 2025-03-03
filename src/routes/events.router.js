7ed1f9e8d235482c361acba13c1131d9251da03e
BlurryQ <jazz.barlow@outlook.com>
2025-03-03 13:37:52 +0000

    Implements new endpoints

--- src/routes/events.router.js
+++ src/routes/events.router.js
@@ -2,14 +2,20 @@ const {
   getAllEvents,
   getEventByID,
   createEvent,
+  editEvent,
+  removeEvent,
 } = require('../controllers/events.controller');
 
 const eventsRouter = require('express').Router();
 
 eventsRouter.get('/', getAllEvents);
 
+eventsRouter.post('/', createEvent);
+
 eventsRouter.get('/:eventID', getEventByID);
 
-eventsRouter.post('/', createEvent);
+eventsRouter.patch('/:eventID', editEvent);
+
+eventsRouter.delete('/:eventID', removeEvent);
 
 module.exports = { eventsRouter };
