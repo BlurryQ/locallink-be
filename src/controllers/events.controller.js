16de7ad2ede61f946999d7437c428f22ea0f3f44
BlurryQ <jazz.barlow@outlook.com>
2025-03-03 13:38:39 +0000

    Improves readability and implements PATCH and DELETE

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -3,29 +3,24 @@ const {
   selectAllEvents,
   selectEventByID,
   postEvent,
+  patchEvent,
+  deleteEvent,
 } = require('../models/events.model');
 const { formatEventData } = require('../utils/formatEventData');
-const { haversineDistance } = require('../utils/haversineDistance');
+const { getEventsWithinRadius } = require('../utils/getEventsWithinRadius');
 
 exports.getAllEvents = async (req, res) => {
-  const { category, lat, long, radius, isFree } = req.query;
+  const { category, lat, long, radius, isFree, recent } = req.query;
   try {
-    const data = await selectAllEvents(category, isFree);
+    const data = await selectAllEvents(category, isFree, recent);
     let events = data.documents.map(formatEventData);
     // if location query and all data present
-    // TODO abstraction function
     if (lat && long && radius) {
       const currentCoords = { lat, long };
-      events = events.filter((event) => {
-        event.location = JSON.parse(event.location);
-        const distanceFromLocation = haversineDistance(
-          currentCoords,
-          event.location.coords
-        );
-        if (distanceFromLocation <= radius) return event;
-      });
+      events = getEventsWithinRadius(currentCoords, events, radius);
     }
-    const allEvents = { events, total: data.total };
+    const total = events.length;
+    const allEvents = { events, total };
     res.status(200).send(allEvents);
   } catch (err) {
     // console.error('Error fetching events:', err);
@@ -48,11 +43,37 @@ exports.getEventByID = async (req, res) => {
 exports.createEvent = async (req, res) => {
   try {
     const eventData = req.body;
+    eventData.location = JSON.stringify(eventData.location);
     const newEventData = await postEvent(eventData);
     const newEvent = formatEventData(newEventData);
     res.status(201).send(newEvent);
   } catch (err) {
     // console.error('Error posting event:', err);
+    res.status(400).send({ error: err.message });
+  }
+};
+
+exports.editEvent = async (req, res) => {
+  try {
+    const { eventID } = req.params;
+    const eventData = req.body;
+    eventData.location = JSON.stringify(eventData.location);
+    const updatedEventData = await patchEvent(eventID, eventData);
+    const updatedEvent = formatEventData(updatedEventData);
+    res.status(200).send(updatedEvent);
+  } catch (err) {
+    // console.error('Error updating event:', err);
+    res.status(400).send({ error: err.message });
+  }
+};
+
+exports.removeEvent = async (req, res) => {
+  try {
+    const { eventID } = req.params;
+    await deleteEvent(eventID);
+    res.status(204).send();
+  } catch (err) {
+    // console.error('Error deleting event:', err);
     res.status(400).send({ error: err.message });
   }
 };
