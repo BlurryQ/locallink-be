14c2f1c11484b83c37cefa4c3dfdbea9c9c49429
BlurryQ <jazz.barlow@outlook.com>
2025-03-04 10:42:45 +0000

    Adds endpoints

--- src/routes/tickets.router.js
+++ src/routes/tickets.router.js
@@ -1,11 +1,20 @@
 const {
   getAllTickets,
   getTicketByID,
+  createTicket,
+  editTicket,
+  removeTicket,
 } = require('../controllers/tickets.controller');
 const ticketsRouter = require('express').Router();
 
 ticketsRouter.get('/', getAllTickets);
 
+ticketsRouter.post('/', createTicket);
+
 ticketsRouter.get('/:ticketID', getTicketByID);
+
+ticketsRouter.patch('/:ticketID', editTicket);
+
+ticketsRouter.delete('/:ticketID', removeTicket);
 
 module.exports = { ticketsRouter };
