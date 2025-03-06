6329be1ffc69c32881a1bc5241744133b7892140
BlurryQ <jazz.barlow@outlook.com>
2025-03-06 12:36:10 +0000

    Finalises tests

--- __tests__/ticketsEvents.test.js
+++ __tests__/ticketsEvents.test.js
@@ -4,9 +4,9 @@ require('jest-sorted');
 
 describe('/tickets/events/:id endpoint testing for LocalLink', () => {
   describe('GET /tickets/events/:owner_id - returns an array of objects containing ticket information', () => {
-    it('200: returns an array', () => {
+    it('200: returns an array of ticket objects filtered by "owner_id"', () => {
       return request(app)
-        .get('/tickets/events/1')
+        .get('/tickets/events/' + 1)
         .expect(200)
         .then(({ body }) => {
           const tickets = body.tickets;
@@ -15,7 +15,7 @@ describe('/tickets/events/:id endpoint testing for LocalLink', () => {
     });
     it('200: returns the appropriate object shape', () => {
       return request(app)
-        .get('/tickets/events/1')
+        .get('/tickets/events/' + 1)
         .expect(200)
         .then(({ body }) => {
           const event = body.tickets[0];
@@ -25,67 +25,21 @@ describe('/tickets/events/:id endpoint testing for LocalLink', () => {
           expect(event).toHaveProperty('location');
           expect(event).toHaveProperty('status');
           expect(event).toHaveProperty('image_url');
+          expect(event).toHaveProperty('total_tickets');
         });
     });
-    it('200: returns the total of ticket objects correctly (3)', () => {
+    it('200: returns the total of ticket objects correctly (2)', () => {
       return request(app)
-        .get('/tickets/events/1')
+        .get('/tickets/events/' + 1)
         .expect(200)
         .then(({ body }) => {
           expect(body.tickets.length).toBe(2);
           expect(body.total).toBe(2);
         });
     });
-    // TODO fix below
-    it("200: returns the ticket objects filtered by 'owner_id'", () => {
-      const returnArr = [
-        {
-          owner_id: 1,
-          price: 100,
-        },
-        {
-          owner_id: 1,
-          price: 200,
-        },
-      ];
+    it("200: returns empty array if no tickets are found or user doesn't exist", () => {
       return request(app)
-        .get('/tickets/events/1' + '?owner_id=1')
-        .expect(200)
-        .then(({ body }) => {
-          expect(Array.isArray(body.tickets)).toBe(true);
-          expect(body.tickets[0]).toHaveProperty('id');
-          expect(body.tickets[0]).toHaveProperty('event_id');
-          expect(body.tickets[0]).toHaveProperty('purchased');
-          const ticketsWithoutPurchaseDateAndID = body.tickets.map(
-            ({ purchased, id, event_id, ...rest }) => rest
-          );
-          expect(ticketsWithoutPurchaseDateAndID).toEqual(returnArr);
-        });
-    });
-    it("200: returns the ticket objects filtered by 'price'", () => {
-      const returnArr = [
-        {
-          owner_id: 1,
-          price: 100,
-        },
-      ];
-      return request(app)
-        .get('/tickets/events/1' + '?price=100')
-        .expect(200)
-        .then(({ body }) => {
-          expect(Array.isArray(body.tickets)).toBe(true);
-          expect(body.tickets[0]).toHaveProperty('id');
-          expect(body.tickets[0]).toHaveProperty('event_id');
-          expect(body.tickets[0]).toHaveProperty('purchased');
-          const ticketsWithoutPurchaseDateAndID = body.tickets.map(
-            ({ purchased, id, event_id, ...rest }) => rest
-          );
-          expect(ticketsWithoutPurchaseDateAndID).toEqual(returnArr);
-        });
-    });
-    it('200: returns empty array if no tickets are found', () => {
-      return request(app)
-        .get('/tickets/events/1' + '?owner_id=3')
+        .get('/tickets/events/' + 3)
         .expect(200)
         .then(({ body }) => {
           expect(body.tickets.length).toBe(0);
