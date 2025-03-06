7cdc0c7d3ed0691c4e319d6e2afb2191a8d1c9bf
BlurryQ <jazz.barlow@outlook.com>
2025-03-06 13:50:42 +0000

    Implements docs on root

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const apiRouter = require('express').Router();
-
 const { eventsRouter } = require('./events.router');
 const { ticketsRouter } = require('./tickets.router');
+const docs = require('../../docs.json');
 
 apiRouter.use('/events', eventsRouter);
 
 apiRouter.use('/tickets', ticketsRouter);
+
+apiRouter.use('/', (req, res) => {
+  res.status(200).send({ docs });
+});
 
 module.exports = { apiRouter };
