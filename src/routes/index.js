094281d0da2a820df6e7dc1c4b9b7387f6efe923
BlurryQ <jazz.barlow@outlook.com>
2025-03-07 19:44:34 +0000

    Implements docs route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,22 @@
-const apiRouter = require('express').Router();
+const express = require('express');
+const apiRouter = express.Router();
 const { eventsRouter } = require('./events.router');
 const { ticketsRouter } = require('./tickets.router');
-const docs = require('../../docs.json');
+const { docsRouter } = require('./docs.router');
 
 apiRouter.use('/events', eventsRouter);
 
 apiRouter.use('/tickets', ticketsRouter);
 
-apiRouter.use('/', (req, res) => {
-  res.status(200).send({ docs });
+apiRouter.get('/', docsRouter);
+
+apiRouter.all('*', (req, res) => {
+  const errorMessage = {
+    status: 404,
+    message: 'Resource not found',
+    path: req.originalUrl,
+  };
+  res.status(404).send(errorMessage);
 });
 
 module.exports = { apiRouter };
