9f1a39a0540df564860f6f6fbad91390a0e18c8d
BlurryQ <jazz.barlow@outlook.com>
2025-03-16 13:26:26 +0000

    Adds function to give string function to tickets

--- db/seeds/seedDatabase.js
+++ db/seeds/seedDatabase.js
@@ -13,18 +13,27 @@ const debugging = false;
 const seedCollection = async (collectionID, dataPath) => {
   const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
   // count is used to assign ids programmatically (eg 1, 2 and 2)
-  let count = 0;
+  let eventIdIndex = 0;
+  let i = 0;
   for (const item of data) {
-    // if event data stringify location obj
+    // if event data, stringify location obj
     if (item.location) item.location = JSON.stringify(item.location);
-    // if ticket data match int to string uid
+    // if ticket data, match int to string uid
     else if (item.owner_id) {
       // get data from events table and match
       const ticket = await databases.listDocuments(
         process.env.APPWRITE_DATABASE_ID,
         process.env.APPWRITE_EVENTS_TABLE
       );
-      item.event_id = ticket.documents[count].$id;
+      item.event_id = ticket.documents[eventIdIndex].$id;
+
+      // get data from users table and match
+      const owners = await databases.listDocuments(
+        process.env.APPWRITE_DATABASE_ID,
+        process.env.APPWRITE_USERS_TABLE
+      );
+      item.owner_id =
+        i === 2 ? owners.documents[1].$id : owners.documents[0].$id;
     }
     const res = await databases.createDocument(
       process.env.APPWRITE_DATABASE_ID,
@@ -32,7 +41,8 @@ const seedCollection = async (collectionID, dataPath) => {
       'unique()',
       item
     );
-    count = 1;
+    eventIdIndex = 1;
+    i++;
   }
 };
 
@@ -43,16 +53,18 @@ const seedDatabase = async () => {
       './db/data/events.json'
     );
     if (debugging) console.log('✅ Events seeded!');
-    await seedCollection(
-      process.env.APPWRITE_TICKETS_TABLE,
-      './db/data/tickets.json'
-    );
-    if (debugging) console.log('✅ Tickets seeded!');
+
     await seedCollection(
       process.env.APPWRITE_USERS_TABLE,
       './db/data/users.json'
     );
     if (debugging) console.log('✅ Users seeded!');
+
+    await seedCollection(
+      process.env.APPWRITE_TICKETS_TABLE,
+      './db/data/tickets.json'
+    );
+    if (debugging) console.log('✅ Tickets seeded!');
 
     if (debugging) console.log('✅ Database seeded! ✅');
   } catch (err) {
